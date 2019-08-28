198bbc62ef211b6b97c4ab458e94d0fa74d2ac54
Yevhenii Sen <jeximst@gmail.com>
2019-08-28 18:29:36 +0300

    fix

--- config/database.js
+++ config/database.js
@@ -7,12 +7,8 @@ module.exports = {
     dialect: "postgres"
   },
   test: {
-    username: "haryktmmcnxchg",
-    password: "32907fb8ad2ad5ea2e06152d93e0d122b1f1b3f97794db5993312f9156bb5a7e",
-    database: "d3jnovf2l8f43c",
-    host: "ec2-46-137-187-23.eu-west-1.compute.amazonaws.com",
-    dialect: "postgres",
-    ssl: true
+    url: "postgres://haryktmmcnxchg:32907fb8ad2ad5ea2e06152d93e0d122b1f1b3f97794db5993312f9156bb5a7e@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/d3jnovf2l8f43c",
+    dialect: "postgres"
   },
   production: {
     url: "postgres://haryktmmcnxchg:32907fb8ad2ad5ea2e06152d93e0d122b1f1b3f97794db5993312f9156bb5a7e@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/d3jnovf2l8f43c",
