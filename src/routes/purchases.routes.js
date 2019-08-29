c2a5937696ac44fec0acb1bbe2b966a071541c75
Yevhenii Sen <jeximst@gmail.com>
2019-08-29 13:04:18 +0300

    Public purchases list

--- src/routes/purchases.routes.js
+++ src/routes/purchases.routes.js
@@ -4,7 +4,7 @@ import * as UsersMiddleware from "../middlewares/users.middleware";
 
 const router = Router();
 
-router.get("/", UsersMiddleware.validateUser, PurchasesController.purchasesList);
+router.get("/", PurchasesController.purchasesList);
 router.post("/", UsersMiddleware.validateUser, PurchasesController.createPurchase);
 
 export default router;
